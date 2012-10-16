68164a71f2ee184852d6002eab5edf1dead26b35
Alex Clark <aclark@aclark.net>
2012-10-16 18:29:50 -0400

    Add manifest, package, setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION='0.0.1'
+
+setup(
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    include_package_data=True,
+    name='django-pci-auth',
+    version=VERSION,
+)
