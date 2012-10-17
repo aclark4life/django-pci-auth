150bf11ee086fd51aff09e02f3a51c917361ff50
Alex Clark <aclark@aclark.net>
2012-10-17 09:39:05 -0400

    Add view

--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, url, include
+from django_pci_auth import views
 
 urlpatterns = patterns('',
-    (r'^articles/2003/$', 'news.views.special_case_2003'),
-    (r'^articles/(\d{4})/$', 'news.views.year_archive'),
-    (r'^articles/(\d{4})/(\d{2})/$', 'news.views.month_archive'),
-    (r'^articles/(\d{4})/(\d{2})/(\d+)/$', 'news.views.article_detail'),
+    (r'', views.current_datetime),
 )
