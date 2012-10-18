bbcbd67d6f0b00db3e6860e165b2bf7bd04d9fb5
Alex Clark <aclark@aclark.net>
2012-10-17 23:04:07 -0400

    Add db and admin

--- django_pci_auth/settings.py
+++ django_pci_auth/settings.py
@@ -1,4 +1,13 @@
-# Django settings
+from django.contrib import admin
 from django_pci_auth import urls
-ROOT_URLCONF=urls
-DEBUG=True
+
+ROOT_URLCONF = urls
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase'
+    }
+}
+DEBUG = True
+
+admin.autodiscover()
--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import patterns, url, include
+from django.contrib import admin
+from django.conf.urls import include, patterns, url
 from django_pci_auth import views
 
 urlpatterns = patterns('',
-    (r'', views.current_datetime),
+    (r'', include(admin.site.urls)),
 )
