86e85af17f2dd408e3a41015d12247366ca2287f
Alex Clark <aclark@aclark.net>
2012-10-18 17:55:36 -0400

    New settings based on startproject

--- django_pci_auth/settings.py
+++ django_pci_auth/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'django_pci_auth.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -1,7 +1,28 @@
+
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from django.conf.urls import include, patterns, url
-from django_pci_auth import views
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'', include(admin.site.urls)),
+    # Example:
+    # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+
+    # Hello, world!
+    (r'', 'project.views.index'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
+
