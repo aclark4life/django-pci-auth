c53cc95d785d9246645fbb6991d37c07c66a4565
Alex Clark <aclark@aclark.net>
2012-10-24 11:08:31 -0400

    Restore view this time w/template

--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -2,12 +2,18 @@ from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
 from django.conf import settings
 from django.contrib import admin
+from django_pci_auth.views import index
+import django.contrib.admindocs.urls
+
+
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
-    (r'^doc/', include('django.contrib.admindocs.urls')),
-    (r'', include(admin.site.urls)),
+urlpatterns = patterns(
+    '',  # prefix
+    (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include(django.contrib.admindocs.urls)),
+    (r'', index),
 )
 
 if settings.DEBUG:
