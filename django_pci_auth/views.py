a427f015830ff5daf2ff2c9aa2165d242d6ba904
Alex Clark <aclark@aclark.net>
2012-10-24 12:11:59 -0400

    Add context

--- django_pci_auth/views.py
+++ django_pci_auth/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 
 def index(request):
-    return render_to_response('django_pci_auth.html')
+    return render_to_response('django_pci_auth.html',
+        {'context': RequestContext(request)})
