9f72095ca5c3ed7335cc682b36a4488f9deb001f
Alex Clark <aclark@aclark.net>
2012-10-24 14:42:04 -0400

    Uncomment

--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
 from django.conf import settings
-#from django.contrib.auth.views import password_change
+from django.contrib.auth.views import password_change
 from django.contrib import admin
 from django_pci_auth.views import index
-#from django_pci_auth.forms import ValidatingPasswordChangeForm
+from django_pci_auth.forms import ValidatingPasswordChangeForm
 import django.contrib.admindocs.urls
 
 admin.autodiscover()
@@ -13,8 +13,8 @@ urlpatterns = patterns(
     '',  # prefix
     (r'^admin/', include(admin.site.urls)),
     (r'^admin/doc/', include(django.contrib.admindocs.urls)),
-#    (r'^change_password/$', password_change,
-#        {'password_change_form': ValidatingPasswordChangeForm}),
+    (r'^change_password/$', password_change,
+        {'password_change_form': ValidatingPasswordChangeForm}),
     (r'', index),
 )
 
