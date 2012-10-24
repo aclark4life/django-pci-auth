3a63ac2777a12ccaa28dee2a2afec2f1863251a1
Alex Clark <aclark@aclark.net>
2012-10-24 17:00:11 -0400

    Fix doc url pattern

--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -14,8 +14,8 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',  # prefix
-    (r'^admin/', include(admin.site.urls)),
     (r'^admin/doc/', include(django.contrib.admindocs.urls)),
+    (r'^admin/', include(admin.site.urls)),
     (r'^change_password/$', password_change,
         {'password_change_form': ValidatingPasswordChangeForm}),
     (r'^password_changed/$', django.contrib.auth.views.password_change_done),
