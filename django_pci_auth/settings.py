c4e24d1fc08fa7267d3b9522e2de96dd777dc160
Alex Clark <aclark@aclark.net>
2012-10-23 21:55:44 -0400

    Clean up and flake8

--- django_pci_auth/settings.py
+++ django_pci_auth/settings.py
@@ -1,7 +1,7 @@
 import os
 
-DEBUG=True
-TEMPLATE_DEBUG=DEBUG
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 # Django settings for django_pci_auth project.
 ADMINS = (
@@ -12,12 +12,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django_pci_auth.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'django_pci_auth.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -104,9 +104,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'django_pci_auth.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.dirname(__file__), "templates"),
 )
 
@@ -146,11 +143,11 @@ LOGGING = {
     }
 }
 
-# stronger password hashing that allows run time selection of hashing algoritm scrypt, bcrypt, PBKDF2, etc. via settings.py
+# stronger password hashing that allows run time selection of hashing
+# algoritm scrypt, bcrypt, PBKDF2, etc. via settings.py
 PASSWORD_MIN_LENGTH = 6
 PASSWORD_MAX_LENGTH = None
 PASSWORD_DICTIONARY = None
 PASSWORD_MATCH_THRESHOLD = 0.9
-#PASSWORD_COMMON_SEQUENCES = 
+#PASSWORD_COMMON_SEQUENCES =
 PASSWORD_COMPLEXITY = None
-
--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -1,32 +1,17 @@
-
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import include
+from django.conf.urls.defaults import patterns
 from django.conf import settings
-#from django_pci_auth.admin import admin_site
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    #(r'^doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #(r'^admin/', include(admin.site.urls)),
+    (r'^doc/', include('django.contrib.admindocs.urls')),
     (r'', include(admin.site.urls)),
-    #(r'', include(admin_site.urls)),
-
-    # Hello, world!
-    #(r'', 'django_pci_auth.views.index'),
 )
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
+    urlpatterns += patterns(
+        '',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-         {'document_root': settings.MEDIA_ROOT}),
+            {'document_root': settings.MEDIA_ROOT}),
     )
-
