43963363a22a072eedbca02d368604c73515e130
Alex Clark <aclark@aclark.net>
2012-10-24 15:12:51 -0400

    Add url to conf and link to change pass form

--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
 from django.conf import settings
 from django.contrib.auth.views import password_change
+from django.contrib.auth.views import password_change_done
 from django.contrib import admin
 from django_pci_auth.views import index
 from django_pci_auth.forms import ValidatingPasswordChangeForm
@@ -15,6 +16,7 @@ urlpatterns = patterns(
     (r'^admin/doc/', include(django.contrib.admindocs.urls)),
     (r'^change_password/$', password_change,
         {'password_change_form': ValidatingPasswordChangeForm}),
+    (r'^password_changed/$', django.contrib.auth.views.password_change_done),
     (r'', index),
 )
 
