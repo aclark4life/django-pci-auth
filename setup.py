c674ac14c7986fc26db887bbb5e6bfae54a4ec41
Alex Clark <aclark@aclark.net>
2012-10-26 18:21:40 -0400

    See if adding django works around django-axes

--- setup.py
+++ setup.py
@@ -7,13 +7,17 @@ VERSION='0.0.1'
 setup(
     author='Alex Clark',
     author_email='aclark@aclark.net',
+    description='PCI-compliant authentication application for Django 1.4+. '
+        'Uses "best of" existing libraries then fills in the gaps.',
     include_package_data=True,
     install_requires=[
         'docutils',
+        'Django>=1.4',
         'django-axes',
         'django-passwords',
         'py-bcrypt',
     ],
+    long_description=open("README.rst").read(),
     name='django-pci-auth',
     version=VERSION,
 )
