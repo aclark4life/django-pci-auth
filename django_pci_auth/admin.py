a5b3b41225e2eca047a7267378df5223a4ee1440
Alex Clark <aclark@aclark.net>
2012-11-16 12:52:43 -0500

    Add files

--- django_pci_auth/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
