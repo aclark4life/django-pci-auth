96020152a76fa128e9e70a285e866f27278d266a
Alex Clark <aclark@aclark.net>
2012-12-03 09:04:28 -0500

    Or not

--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -14,12 +14,12 @@ dajaxice_autodiscover()
 
 urlpatterns = patterns(
     '',  # prefix
-    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     (r'^admin/doc/', include(django.contrib.admindocs.urls)),
     (r'^admin/', include(admin.site.urls)),
     (r'^change_password/$', password_change,
         {'password_change_form': ValidatingPasswordChangeForm,
         'post_change_redirect' : '/password_changed/'}),
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     (r'^password_changed/$', password_change_done),
     (r'', index),
 )
