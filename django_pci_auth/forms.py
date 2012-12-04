bb0318e0ba7e7474f812bf2da7f0af1a1ccb061e
Alex Clark <aclark@aclark.net>
2012-12-04 12:10:36 -0500

    Flake8

--- django_pci_auth/forms.py
+++ django_pci_auth/forms.py
@@ -3,9 +3,10 @@ from django.contrib.auth.forms import SetPasswordForm
 from django.utils.translation import ugettext_lazy as _
 from passwords.fields import PasswordField
 
- 
+
 class ValidatingSetPasswordForm(SetPasswordForm):
     new_password2 = PasswordField(label=_("New password confirmation"))
- 
+
+
 class ValidatingPasswordChangeForm(PasswordChangeForm):
     new_password2 = PasswordField(label=_("New password confirmation"))
