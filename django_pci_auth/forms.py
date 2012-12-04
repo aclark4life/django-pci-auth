b7ea04035954f3644192bd3762cb81f838845fa9
Alex Clark <aclark@aclark.net>
2012-12-04 08:10:41 -0500

    Start over

--- django_pci_auth/forms.py
+++ django_pci_auth/forms.py
@@ -1,41 +1,54 @@
-from django import forms
-from django.conf import settings
-from django.contrib import auth
-from django.contrib.auth.hashers import check_password
-from passwords.validators import PASSWORD_MIN_LENGTH
-from models import PasswordLog
-import utils
+
+from django.contrib.auth.forms import SetPasswordForm, PasswordChangeForm
+from django.utils.translation import ugettext_lazy as _
+from passwords.fields import PasswordField
+ 
+class ValidatingSetPasswordForm(SetPasswordForm):
+    new_password2 = PasswordField(label=_("New password confirmation"))
+ 
+class ValidatingPasswordChangeForm(PasswordChangeForm):
+    new_password2 = PasswordField(label=_("New password confirmation"))
 
 
-# http://stackoverflow.com/questions/5226329/\
-# enforcing-password-strength-requirements-with-django-contrib-auth-views\
-# -password
-class ValidatingPasswordChangeForm(auth.forms.PasswordChangeForm):
-    """
-    Mix and match code from this StackOverflow answer and django-passwords app
-    """
 
-    MIN_LENGTH = PASSWORD_MIN_LENGTH  # Get setting from django-passwords
-
-    def clean_new_password1(self):
-        password1 = self.cleaned_data.get('new_password1')
-
-        # At least MIN_LENGTH long
-#        if utils.check_password_length(password1):
-        raise forms.ValidationError(
-                "The new password must be at least %d characters long." %
-                    self.MIN_LENGTH)
-
-        # At least one letter and one non-letter
-        if not utils.check_password_complexity(password1):
-            raise forms.ValidationError(
-                "The new password must contain at least one letter and at"
-                " least one digit or punctuation character.")
-
-        # Make sure password hasn't been used recently
-        if self.user:
-            if not utils.check_password_recent(password1):
-                raise forms.ValidationError(
-                    "That password was used recently. Please pick a new one.")
-
-        return password1
+#from django import forms
+#from django.conf import settings
+#from django.contrib import auth
+#from django.contrib.auth.hashers import check_password
+#from passwords.validators import PASSWORD_MIN_LENGTH
+#from models import PasswordLog
+#import utils
+#
+#
+## http://stackoverflow.com/questions/5226329/\
+## enforcing-password-strength-requirements-with-django-contrib-auth-views\
+## -password
+#class ValidatingPasswordChangeForm(auth.forms.PasswordChangeForm):
+#    """
+#    Mix and match code from this StackOverflow answer and django-passwords app
+#    """
+#
+#    MIN_LENGTH = PASSWORD_MIN_LENGTH  # Get setting from django-passwords
+#
+#    def clean_new_password1(self):
+#        password1 = self.cleaned_data.get('new_password1')
+#
+#        # At least MIN_LENGTH long
+##        if utils.check_password_length(password1):
+#        raise forms.ValidationError(
+#                "The new password must be at least %d characters long." %
+#                    self.MIN_LENGTH)
+#
+#        # At least one letter and one non-letter
+#        if not utils.check_password_complexity(password1):
+#            raise forms.ValidationError(
+#                "The new password must contain at least one letter and at"
+#                " least one digit or punctuation character.")
+#
+#        # Make sure password hasn't been used recently
+#        if self.user:
+#            if not utils.check_password_recent(password1):
+#                raise forms.ValidationError(
+#                    "That password was used recently. Please pick a new one.")
+#
+#        return password1
--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -1,32 +1,76 @@
-from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+
 from django.conf.urls.defaults import *
 from django.conf import settings
-from django.contrib.auth.views import password_change
+from django.contrib.auth.views import password_change_done
+
+# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from django_pci_auth.views import index, password_change_done
-from django_pci_auth.forms import ValidatingPasswordChangeForm
-import django.contrib.admindocs.urls
-
-
 admin.autodiscover()
-dajaxice_autodiscover()
 
 
-urlpatterns = patterns(
-    '',  # prefix
-    (r'^admin/doc/', include(django.contrib.admindocs.urls)),
+from forms import ValidatingPasswordChangeForm
+
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+    (r'^admin/password_change/$', 'django.contrib.auth.views.password_change', {'password_change_form': ValidatingPasswordChangeForm}),
+    (r'^admin/password_changed/$', password_change_done),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+
+
+    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-    (r'^change_password/$', password_change,
-        {'password_change_form': ValidatingPasswordChangeForm,
-        'post_change_redirect' : '/password_changed/'}),
-    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
-    (r'^password_changed/$', password_change_done),
-    (r'', index),
+
+    # Hello, world!
+    (r'', 'django_pci_auth.views.index'),
+
+
+
+
 )
 
 if settings.DEBUG:
-    urlpatterns += patterns(
-        '',
+    urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.MEDIA_ROOT}),
+         {'document_root': settings.MEDIA_ROOT}),
     )
+
+
+
+#from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+#from django.conf.urls.defaults import *
+#from django.conf import settings
+#from django.contrib.auth.views import password_change
+#from django.contrib import admin
+#from django_pci_auth.views import index, password_change_done
+#from django_pci_auth.forms import ValidatingPasswordChangeForm
+#import django.contrib.admindocs.urls
+#
+#
+#admin.autodiscover()
+#dajaxice_autodiscover()
+#
+#
+#urlpatterns = patterns(
+#    '',  # prefix
+#    (r'^admin/doc/', include(django.contrib.admindocs.urls)),
+#    (r'^admin/', include(admin.site.urls)),
+#    (r'^change_password/$', password_change,
+#        {'password_change_form': ValidatingPasswordChangeForm,
+#        'post_change_redirect' : '/password_changed/'}),
+#    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+#    (r'^password_changed/$', password_change_done),
+#    (r'', index),
+#)
+#
+#if settings.DEBUG:
+#    urlpatterns += patterns(
+#        '',
+#        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+#            {'document_root': settings.MEDIA_ROOT}),
+
+#    )
