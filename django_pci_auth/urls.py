4c621fa390a42b923fb89017774353eb0be2c12b
Alex Clark <aclark@aclark.net>
2012-12-04 08:15:56 -0500

    FINALLY (integrated dajaxice w/django-passwords)

--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -1,3 +1,4 @@
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib.auth.views import password_change_done
@@ -9,6 +10,7 @@ from forms import ValidatingPasswordChangeForm
 
 
 urlpatterns = patterns('',
+    (dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     (r'^admin/password_change/$', 'django.contrib.auth.views.password_change', {'password_change_form': ValidatingPasswordChangeForm}),
     (r'^admin/password_changed/$', password_change_done),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
