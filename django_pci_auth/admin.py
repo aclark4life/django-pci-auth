5e782b504297561e15bbee611ccbf8e210b7ee08
Alex Clark <aclark@aclark.net>
2012-12-04 12:10:06 -0500

    Flake8

--- django_pci_auth/admin.py
+++ django_pci_auth/admin.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 from django_pci_auth.models import UserProfile, PasswordLog
 
 
-# https://docs.djangoproject.com/en/dev/topics/auth/#storing-additional-information-about-users
+# https://docs.djangoproject.com/en/dev/topics/auth/\
+# #storing-additional-information-about-users
 # Define an inline admin descriptor for UserProfile model
 # which acts a bit like a singleton
 class UserProfileInline(admin.StackedInline):
@@ -12,18 +13,19 @@ class UserProfileInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'profile'
 
+
 # Define a new User admin
 class UserAdmin(UserAdmin):
     inlines = (UserProfileInline, )
 
-# Re-register UserAdmin
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
 
 class PasswordLogAdmin(admin.ModelAdmin):
-    list_display = ('user','password', 'create_date')
+    list_display = ('user', 'password', 'create_date')
     list_filter = ['user', 'create_date']
     search_fields = ['user', 'create_date']
     date_hierarchy = 'create_date'
 
+
+admin.site.unregister(User)  # Re-register UserAdmin
+admin.site.register(User, UserAdmin)
 admin.site.register(PasswordLog, PasswordLogAdmin)
