8039a83cfd8b55dcc06902b621ec11b3f5341bbf
Alex Clark <aclark@aclark.net>
2012-12-04 10:14:24 -0500

    Reorg settings

--- django_pci_auth/settings.py
+++ django_pci_auth/settings.py
@@ -6,17 +6,6 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 AUTH_PROFILE_MODULE = 'django_pci_auth.UserProfile'
-
-
-# django-axes defaults
-AXES_LOGIN_FAILURE_LIMIT = 4
-AXES_LOCK_OUT_AT_FAILURE = True
-AXES_USE_USER_AGENT = False
-AXES_COOLOFF_TIME = None
-#AXES_LOGGER = axes.watch_login
-AXES_LOCKOUT_TEMPLATE = None
-AXES_LOCKOUT_URL = None
-AXES_VERBOSE = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -29,7 +18,6 @@ DATABASES = {
 }
 DEBUG = True
 INSTALLED_APPS = (
-    'dajaxice',  # django-dajaxice
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -42,7 +30,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
 
+    # Django PCI Auth
     'axes',  # django-axes
+    'dajaxice',  # django-dajaxice
     'django_pci_auth',
     'passwords',  # django-passwords
 )
@@ -76,30 +66,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'axes.middleware.FailedLoginMiddleware',
 )
-
-# how many old password do you want to store, so they can't be reused.
-OLD_PASSWORD_STORAGE_NUM = 4
-
-# django-passwords defaults
-PASSWORD_MIN_LENGTH = 6
-PASSWORD_MAX_LENGTH = None
-PASSWORD_DICTIONARY = None
-PASSWORD_MATCH_THRESHOLD = 0.9
-#PASSWORD_COMMON_SEQUENCES =
-PASSWORD_COMPLEXITY = None
-PASSWORD_HASHERS = (  # Django 1.4 password hashing algorithms
-    # From https://docs.djangoproject.com/en/1.4/topics/auth/:
-    # "[redacted] This means that Django will use the first hash in the list
-    # to store all passwords, but will support checking passwords stored with
-    # the rest of the hashes in the list. If you remove a hash from the list
-    # it will no longer be supported.
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-    'django.contrib.auth.hashers.CryptPasswordHasher',
-)
 ROOT_URLCONF = 'django_pci_auth.urls'
 SITE_ID = 1
 STATIC_ROOT = ''
@@ -113,8 +79,6 @@ STATICFILES_FINDERS = (
     'dajaxice.finders.DajaxiceFinder',
 )
 SECRET_KEY = 'abc123'
-# From https://docs.djangoproject.com/en/1.4/topics/http/sessions/
-SESSION_COOKIE_AGE = 7200
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -137,3 +101,43 @@ TEMPLATE_DIRS = (
 TIME_ZONE = 'America/Chicago'
 USE_I18N = True
 USE_L10N = True
+
+
+# Django PCI Auth Settings
+PASSWORD_HASHERS = (  # Django 1.4 password hashing algorithms
+    # From https://docs.djangoproject.com/en/1.4/topics/auth/:
+    # "[redacted] This means that Django will use the first hash in the list
+    # to store all passwords, but will support checking passwords stored with
+    # the rest of the hashes in the list. If you remove a hash from the list
+    # it will no longer be supported.
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
+# From https://docs.djangoproject.com/en/1.4/topics/http/sessions/
+SESSION_COOKIE_AGE = 7200
+
+# django-axes defaults
+AXES_LOGIN_FAILURE_LIMIT = 4
+AXES_LOCK_OUT_AT_FAILURE = True
+AXES_USE_USER_AGENT = False
+AXES_COOLOFF_TIME = None
+#AXES_LOGGER = axes.watch_login
+AXES_LOCKOUT_TEMPLATE = None
+AXES_LOCKOUT_URL = None
+AXES_VERBOSE = True
+
+# how many old password do you want to store, so they can't be reused.
+OLD_PASSWORD_STORAGE_NUM = 4
+
+# django-passwords defaults
+PASSWORD_MIN_LENGTH = 6
+PASSWORD_MAX_LENGTH = None
+PASSWORD_DICTIONARY = None
+PASSWORD_MATCH_THRESHOLD = 0.9
+#PASSWORD_COMMON_SEQUENCES =
+PASSWORD_COMPLEXITY = None
