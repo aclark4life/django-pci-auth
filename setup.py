519d56dc913540bfd87967cc0dbb1ddb096634f0
Alex Clark <aclark@aclark.net>
2014-10-06 16:01:36 -0400

    Update

--- setup.py
+++ setup.py
@@ -2,21 +2,18 @@ from setuptools import find_packages
 from setuptools import setup
 import os
 
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 
 setup(
     author='Alex Clark',
     author_email='aclark@aclark.net',
-    description='Integrates existing PCI-related applications and provides additional PCI-related features.',
+    description='Provides PCI-related password features.',
     include_package_data=True,
     install_requires=[
         'Django>=1.4',
         'django-axes>=1.2.6',
-        'django-admin-bootstrapped',
         'django-dajaxice',
         'django-passwords',
-        'django-password-policies',
-        'docutils',
         'py-bcrypt',
         'pytz',
     ],
