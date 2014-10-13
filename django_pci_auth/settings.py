9d233c20f8e7c7fd4f48b977509a9290e19fb09d
Alex Clark <aclark@aclark.net>
2014-10-12 20:31:24 -0400

    Re-add modern manage, settings, urls

---
+++ django_pci_auth/settings.py
@@ -0,0 +1,84 @@
+"""
+Django settings for django_pci_auth2 project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '1uh@ft4d(481b7vrou%$199^k25u)c0(hs_9_28&j=wh_bd1_4'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_pci_auth',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'django_pci_auth.urls'
+
+#WSGI_APPLICATION = 'django_pci_auth2.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
--- django_pci_auth/urls.py
+++ django_pci_auth/urls.py
@@ -1,31 +1,10 @@
-from dajaxice.core import dajaxice_autodiscover, dajaxice_config
-from django.conf.urls import patterns, url, include  # Via
-    # http://stackoverflow.com/a/19962822
-from django.conf import settings
-#from django.contrib.auth.views import password_change_done
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
-admin.autodiscover()
-# local
-from forms import ValidatingPasswordChangeForm
-from views import password_change_done
-
 
 urlpatterns = patterns('',
-    (dajaxice_config.dajaxice_url, include('dajaxice.urls')),
-    (r'^admin/password_change/$', 'django.contrib.auth.views.password_change',
-        {
-            'password_change_form': ValidatingPasswordChangeForm,
-            'post_change_redirect': '/admin/password_changed/',
-        }
-    ),
-    (r'^admin/password_changed/$', password_change_done),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
-    (r'', 'django_pci_auth.views.index'),
-)
+    # Examples:
+    # url(r'^$', 'django_pci_auth2.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-         {'document_root': settings.MEDIA_ROOT}),
-    )
+    url(r'^admin/', include(admin.site.urls)),
+)
